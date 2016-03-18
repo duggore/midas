b92d6acd66a69339678c5c99f2d3c0647926e022
oliverpalmer <oliver.palmer@mrc-mbu.cam.ac.uk>
2016-03-18 16:56:18 +0000

    Changed use of bigclam so that its location is not stated but binary must be on path

--- run_for_n_clusters.sh
+++ run_for_n_clusters.sh
@@ -9,7 +9,8 @@ source ~/.bashrc
 
 #/net/zeta/zeta/home/op251/Documents/2year/modified_snap/examples/bigclam/bigclam -i:/mallow/data/2year/mito_graph/midas_input_data/edge_thresholded_human_string_graphs/barabasi_edges/scores_removed_barabasi_graph.txt -c:200 -nt:8
 
-bigclam="/home/op251/Documents/2year/modified_snap/examples/bigclam/bigclam"
+#bigclam="/home/op251/Documents/2year/modified_snap/examples/bigclam/bigclam"
+
 #graph_fname="/mallow/data/2year/mito_graph/midas_input_data/edge_thresholded_human_string_graphs/barabasi_edges/scores_removed_barabasi_graph.txt"
 #graph_fname="/mallow/data/2year/mito_graph/midas_input_data/edge_thresholded_human_string_graphs/all_edges/0.0_threshold/scores_removed_threshold_0.0_human_only_string.txt"
 #graph_fname="/mallow/data/2year/mito_graph/v10_raw_string_downloads/no_scores_string_v10_human_links.txt"
@@ -21,7 +22,7 @@ graph_fname="/mallow/data/2year/mito_graph/v10_raw_string_downloads/no_scores_no
 reformat_communities_py="/mallow/data/2year/mito_graph/midas_input_data/edge_thresholded_human_string_graphs/barabasi_edges/data_processing/reformat_communities_from_snap_output.py"
 
 echo "Running BIGCLAM"
-$bigclam -i:$graph_fname -c:$NCLUSTERS -nt:8
+bigclam -i:$graph_fname -c:$NCLUSTERS -nt:8
 
 #communities_fname="/mallow/data/2year/mito_graph/midas_input_data/edge_thresholded_human_string_graphs/all_edges/0.0_threshold/string_reformatted_"$NCLUSTERS"_communities.txt"
 #communities_fname="/mallow/data/2year/mito_graph/v10_raw_string_downloads/clustering/string_v10_0.4_threshold_reformatted_"$NCLUSTERS"_communities.txt"
